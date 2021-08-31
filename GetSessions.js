00aac4fc8fee90a2c70e7a82e919a0381eb4a14a
nithyamani <nithya.mn999@gmail.com>
2021-08-31 20:24:52 +0530

    minor updates

--- GetSessions.js
+++ GetSessions.js
@@ -12,7 +12,7 @@ module.exports.getSession = function(build_name, basicAuthCreds, callback){
 		console.log('Build Name: '+build_name);
 		console.log('Build ID: '+build_id); 
 
-		console.log('Please find the generated report in ./reports/report-'+build_name+'.html');
+		console.log('The report has been generated in ./reports/report-'+build_name+'.html');
 		request(
 			{
 				url: 'https://api.browserstack.com/automate/builds/'+build_id+'/sessions.json?limit=100',
@@ -44,7 +44,7 @@ module.exports.getSession = function(build_name, basicAuthCreds, callback){
 					       + '<h3><center>Build Name: '+build_name+'</center></h3>'
 					       + '<h3><center>Build ID: '+build_id+'</center></h3>'
 					       + '<p><center><strong>NOTE: </strong>This report can only fetch a maximum of 100 builds and 100 sessions within each build.</center></p>'
-					       + '<table style="width:100%"><thead><th style="height:50px">Name</th><th>Duration</th><th>OS</th><th>OS Version</th>'
+					       + '<table style="width:100%"><thead><th style="height:50px">Name</th><th>Duration (s)</th><th>OS</th><th>OS Version</th>'
 					       + '<th>Browser</th><th>Browser Version</th><th>Status</th><th>Video URL</th><th>Session ID</th><th>Public Session URL</th></thead>'
 					       + '<tbody>';
 				for(const index in body){
